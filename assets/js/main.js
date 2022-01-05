a1d95575ed37ca426a21a2c4ccf583211f8126df
Julian Reis <julyanreiss@hotmail.com>
2022-01-05 17:25:04 -0400

    feedback parceiros, correção de imagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,3 +99,35 @@ class ValidaFormulario {
 }
 
 const valida = new ValidaFormulario();
+
+/*MODAL JAVASCRIPT */
+
+
+function myModal() {
+    // Obtenha o modal
+    var modal = document.getElementById("myModal");
+
+
+    var btn = document.getElementById("link_modal");
+
+    // Obtenha o elemento <span> que fecha o modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Quando o usuário clica no botão, abre o modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Quando o usuário clicar em <span> (x), feche o modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Quando o usuário clicar em qualquer lugar fora do modal, feche-o
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+myModal();
